ecf5a74995ca0c01a8e9fbc8fc32407a58a97b29
Soukaradi <124304996+Soukaradi@users.noreply.github.com>
2024-02-03 03:36:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import pandas as pd
 import streamlit as st
 st.title("Sales streamlit dashboard")
 st.markdown("_Prototype v0.4.1_")
+
+@st.cache_data
 def load_data(path: str):
     df = pd.read_excel(path)
     return df
+df = load_data(uploaded_file)
